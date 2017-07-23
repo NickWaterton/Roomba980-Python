4198e196f84873f4304fc8ab66e95bc23205ea1c
Philipp Schmitt <pschmitt@users.noreply.github.com>
2017-07-23 04:50:46 +0200

    Packaging (#6)

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='Roomba980-Python',
+    version='1.2.1',
+    license='MIT',
+    description='Python program and library to control iRobot Roomba 980 ' \
+    'Vacuum Cleaner',
+    author_email='nick.waterton@med.ge.com',
+    url='https://github.com/NickWaterton/Roomba980-Python',
+    packages=find_packages(),
+    install_requires=['numpy', 'opencv-python', 'paho-mqtt', 'pillow', 'six'],
+    entry_points={
+        'console_scripts': [
+            'roomba=roomba.roomba:main',
+            'roomba-getpassword=roomba.getpassword:main'
+        ]
+    }
+)
