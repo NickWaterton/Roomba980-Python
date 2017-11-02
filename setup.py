bcf336b653bf1ff315f57f5a45579e0594324ec7
Philipp Schmitt <philipp@schmitt.co>
2017-11-02 14:50:37 +0100

    Bump version to 1.4.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='roombapy',
-    version='1.3.1-1',
+    version='1.4.0',
     license='MIT',
     description='Python program and library to control Wi-Fi enabled iRobot '
                 'Roomba vacuum cleaners',
