a2e4b5008d7fae4b5d665dd316edb3729ce5ba95
Philipp Schmitt <philipp@schmitt.co>
2019-12-14 23:50:16 +0100

    BLACK! There will be no survivors.

--- setup.py
+++ setup.py
@@ -2,21 +2,21 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name='roombapy',
-    version='1.4.2',
-    license='MIT',
-    description='Python program and library to control Wi-Fi enabled iRobot '
-                'Roomba vacuum cleaners',
-    author_email='nick.waterton@med.ge.com',
-    url='https://github.com/pschmitt/roombapy',
+    name="roombapy",
+    version="1.4.2",
+    license="MIT",
+    description="Python program and library to control Wi-Fi enabled iRobot "
+    "Roomba vacuum cleaners",
+    author_email="nick.waterton@med.ge.com",
+    url="https://github.com/pschmitt/roombapy",
     packages=find_packages(),
-    install_requires=['paho-mqtt', 'six'],
-    extras_require={'mapping': ['numpy', 'opencv-python', 'pillow']},
+    install_requires=["paho-mqtt", "six"],
+    extras_require={"mapping": ["numpy", "opencv-python", "pillow"]},
     include_package_data=True,
     entry_points={
-        'console_scripts': [
-            'roomba=roomba.__main__:main',
-            'roomba-getpassword=roomba.getpassword:main'
+        "console_scripts": [
+            "roomba=roomba.__main__:main",
+            "roomba-getpassword=roomba.getpassword:main",
         ]
-    }
+    },
 )
