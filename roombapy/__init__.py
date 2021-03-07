b10d3caf06df80b218a73c695028dba3b71370de
freekode <efrolov.app@gmail.com>
2021-03-07 13:43:47 +0100

    sorted

--- roombapy/__init__.py
+++ roombapy/__init__.py
@@ -1,5 +1,5 @@
 from .discovery import RoombaDiscovery
 from .getpassword import RoombaPassword
 from .roomba import Roomba, RoombaConnectionError
-from .roomba_info import RoombaInfo
 from .roomba_factory import RoombaFactory
+from .roomba_info import RoombaInfo
